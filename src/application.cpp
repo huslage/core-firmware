dc837c8d87c96c7203c7610dc5670b5fb0fef622
Aaron Huslage <huslage@gmail.com>
2014-03-27 15:29:00 -0400

    converting to local build

--- src/application.cpp
+++ src/application.cpp
@@ -1,174 +1,126 @@
-/**
- ******************************************************************************
- * @file    application.cpp
- * @authors  Satish Nair, Zachary Crockett and Mohit Bhoite
- * @version V1.0.0
- * @date    05-November-2013
- * @brief   Tinker application
- ******************************************************************************
-  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation, either
-  version 3 of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this program; if not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************
- */
-
-/* Includes ------------------------------------------------------------------*/  
+// This #include statement was automatically added by the Spark IDE.
 #include "application.h"
 
-/* Function prototypes -------------------------------------------------------*/
-int tinkerDigitalRead(String pin);
-int tinkerDigitalWrite(String command);
-int tinkerAnalogRead(String pin);
-int tinkerAnalogWrite(String command);
+int surf(String args);
+void urlDecode(String &input);
 
-/* This function is called once at start up ----------------------------------*/
-void setup()
-{
-	//Setup the Tinker application here
+/* ============== MAIN =====================*/
 
-	//Register all the Tinker functions
-	Spark.function("digitalread", tinkerDigitalRead);
-	Spark.function("digitalwrite", tinkerDigitalWrite);
+ //Use I2C with OLED RESET pin on D4
+ #define OLED_RESET D4
+ Adafruit_SSD1306 oled(OLED_RESET);
 
-	Spark.function("analogread", tinkerAnalogRead);
-	Spark.function("analogwrite", tinkerAnalogWrite);
 
+ unsigned long previousMillis;
+ unsigned long interval = 30000;
+
+ void setup() {
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // init done     
+  oled.clearDisplay();
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.setCursor(0,0);
+  oled.println("Up next,\nsurf report!");
+  oled.display(); // show splashscreen
+
+  Serial.begin(9600);
+  Serial.println("Waiting...");
+  Spark.function("surf",surf);
 }
 
-/* This function loops forever --------------------------------------------*/
-void loop()
-{
-	//This will run in a loop
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;
+    Spark.publish("refresh","2456");
+    Serial.println("Refreshing...");
+  }
 }
 
-/*******************************************************************************
- * Function Name  : tinkerDigitalRead
- * Description    : Reads the digital value of a given pin
- * Input          : Pin 
- * Output         : None.
- * Return         : Value of the pin (0 or 1) in INT type
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerDigitalRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+int surf(String args) {
+  Serial.print("Received: ");
+  Serial.println(args);
+  Serial.print("Size: ");
+  Serial.println(args.length());
+  
+  urlDecode(args);
 
-	if(pin.startsWith("D"))
-	{
-		pinMode(pinNumber, INPUT_PULLDOWN);
-		return digitalRead(pinNumber);
-	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber+10, INPUT_PULLDOWN);
-		return digitalRead(pinNumber+10);
-	}
-	return -2;
+  int idx = args.indexOf("/");
+  String myid = args.substring(0,idx-1);
+
+  char idbuf[5];
+  myid.toCharArray(idbuf, sizeof(idbuf));
+  int id = atoi(idbuf);
+
+  String name;
+  switch (id) {
+    case 2456: 
+      name = "Playa Guiones";
+      break;
+    case 362: 
+      name = "Higgins Beach";
+      break;
+    case 852: 
+      name = "Old Orchard Beach";
+      break;
+    case 401:
+      name = "Topsail Island";
+      break;
+    case 650: 
+      name = "Carolina Beach";
+      break;
+  }
+
+  oled.clearDisplay();
+  oled.setCursor(0,0);
+  
+  args.replace('/','\n');
+  oled.println(name);
+  oled.println(args);
+
+  oled.display();
+  return 1;
 }
 
-/*******************************************************************************
- * Function Name  : tinkerDigitalWrite
- * Description    : Sets the specified pin HIGH or LOW
- * Input          : Pin and value
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerDigitalWrite(String command)
+void urlDecode(String &input)
 {
-	bool value = 0;
-	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	if(command.substring(3,7) == "HIGH") value = 1;
-	else if(command.substring(3,6) == "LOW") value = 0;
-	else return -2;
-
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		digitalWrite(pinNumber, value);
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		digitalWrite(pinNumber+10, value);
-		return 1;
-	}
-	else return -3;
+  input.trim();
+  input.replace("%20", " ");
+  input.replace("+", " ");
+  input.replace(".", "mph");
+  input.replace("@", "ft ");
+  input.replace("_", "");
+  input.replace("%21", "Today: ");
+  input.replace("%7E", "Tomorrow: ");
+  input.replace("%22", "\"");
+  input.replace("%23", "#");
+  input.replace("%24", "$");
+  input.replace("%25", "%");
+  input.replace("%26", "&");
+  input.replace("%27", "\'");
+  input.replace("%28", "(");
+  input.replace("%29", ")");
+  input.replace("%30", "*");
+  input.replace("%31", "+");
+  input.replace("%2C", ",");
+  input.replace("%2E", ".");
+  input.replace("%2F", "/");
+  input.replace("%2C", ",");
+  input.replace("%3A", ":");
+  input.replace("%3A", ";");
+  input.replace("%3C", "<");
+  input.replace("%3D", "=");
+  input.replace("%3E", ">");
+  input.replace("%3F", "?");
+  input.replace("%40", "@");
+  input.replace("%5B", "[");
+  input.replace("%5C", "\\");
+  input.replace("%5D", "]");
+  input.replace("%5E", "^");
+  input.replace("%5F", "-");
+  input.replace("%60", "`");
 }
 
-/*******************************************************************************
- * Function Name  : tinkerAnalogRead
- * Description    : Reads the analog value of a pin
- * Input          : Pin 
- * Output         : None.
- * Return         : Returns the analog value in INT type (0 to 4095)
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerAnalogRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	if(pin.startsWith("D"))
-	{
-		pinMode(pinNumber, INPUT);
-		return analogRead(pinNumber);
-	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber+10, INPUT);
-		return analogRead(pinNumber+10);
-	}
-	return -2;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerAnalogWrite
- * Description    : Writes an analog value (PWM) to the specified pin
- * Input          : Pin and Value (0 to 255)
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerAnalogWrite(String command)
-{
-	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	String value = command.substring(3);
-
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		analogWrite(pinNumber, value.toInt());
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		analogWrite(pinNumber+10, value.toInt());
-		return 1;
-	}
-	else return -2;
-}
