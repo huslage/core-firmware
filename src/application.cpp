10b5ea5a286ad323bf46b40d5b8b60dca47b2ff9
Aaron Huslage <huslage@gmail.com>
2014-04-02 11:19:57 -0400

    fixed buffer size to return correct ID. code cleanup

--- src/application.cpp
+++ src/application.cpp
@@ -3,54 +3,42 @@
 //#include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
+/* ============== MAIN =====================*/
+
+//Use I2C with OLED RESET pin on D4
+#define OLED_RESET D4
+Adafruit_SSD1306 oled(OLED_RESET);
+
+unsigned long previousMillis;
+unsigned long interval = 30000;
+
 int surf(String args);
 void urlDecode(String &input);
 int id;
-char s_id[4] = "0";
+char s_id[5] = "0";
 
-/* ============== MAIN =====================*/
-
- //Use I2C with OLED RESET pin on D4
- #define OLED_RESET D4
- Adafruit_SSD1306 oled(OLED_RESET);
-
-
- unsigned long previousMillis;
- unsigned long interval = 300000;
-
- void setup() {
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  oled.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done     
+void setup() {
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3D);  
   oled.clearDisplay();
   oled.setTextSize(1);
   oled.setTextColor(WHITE);
   oled.setCursor(0,0);
   oled.println("Up next,\nsurf report!");
   oled.display(); // show splashscreen
-
-  Serial.begin(9600);
-  Serial.println("Waiting...");
   Spark.function("surf",surf);
 }
 
 void loop() {
   unsigned long currentMillis = millis();
-  
+
   if(currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;
-    sprintf(s_id,"%d",id);
+    snprintf(s_id,sizeof(s_id),"%d",id);
     Spark.publish("refresh",s_id);
-    Serial.println("Refreshing...");
   }
 }
 
 int surf(String args) {
-  Serial.print("Received: ");
-  Serial.println(args);
-  Serial.print("Size: ");
-  Serial.println(args.length());
-  
   urlDecode(args);
 
   int idx = args.indexOf("/");
@@ -60,29 +48,29 @@ int surf(String args) {
   char idbuf[5];
   myid.toCharArray(idbuf, sizeof(idbuf));
   id = atoi(idbuf);
-  Serial.println(id);
+  
   String name;
   switch (id) {
     case 2456: 
-      name = "Playa Guiones";
-      break;
+    name = "Playa Guiones";
+    break;
     case 362: 
-      name = "Higgins Beach";
-      break;
+    name = "Higgins Beach";
+    break;
     case 852: 
-      name = "Old Orchard Beach";
-      break;
+    name = "Old Orchard Beach";
+    break;
     case 401:
-      name = "Topsail Island";
-      break;
+    name = "Topsail Island";
+    break;
     case 650: 
-      name = "Carolina Beach";
-      break;
+    name = "Carolina Beach";
+    break;
   }
 
   oled.clearDisplay();
   oled.setCursor(0,0);
-  
+
   mydata.replace('/','\n');
   oled.println(name);
   oled.println(mydata);
@@ -129,4 +117,3 @@ void urlDecode(String &input)
   input.replace("%5F", "-");
   input.replace("%60", "`");
 }
-
